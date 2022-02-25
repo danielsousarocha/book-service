e0df8a9f9978e7547d79fd256e48d790157614b8
Daniel Sousa Rocha <daniel.sousa.rocha@gmail.com>
2022-02-24 22:45:18 -0300

    Delete functionality added

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -10,10 +10,36 @@ export class HomePageComponent implements OnInit {
   books: any = [];
 
   constructor(private http: HttpClient) {}
+  message: string = '';
 
   ngOnInit(): void {
     this.http.get('/api/v1/books/').subscribe((data) => {
       this.books = data;
+    });
+  }
+
+  deleteBook(bookId: number): void {
+    this.message = '';
+
+    this.http.delete(`/api/v1/books/${bookId}`).subscribe(
+      (data: any) => {
+        if (data.result) {
+          this.removeBookFromList(bookId);
+          this.message = `Book #${bookId} deleted`;
+        } else {
+          this.message = `The book could not be deleted`;
+        }
+      },
+      (error) => {
+        console.warn(error);
+        this.message = `The book could not be deleted`;
+      }
+    );
+  }
+
+  private removeBookFromList(key: number) {
+    this.books.forEach((value: any, index: number) => {
+      if (value.id == key) this.books.splice(index, 1);
     });
   }
 }
